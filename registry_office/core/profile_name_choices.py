b224b1e046f456fcf9c2c11fec904c7a1dd604d6
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2023-07-24 17:46:37 +0300

    fix choices for profile name and position

--- registry_office/core/profile_name_choices.py
+++ registry_office/core/profile_name_choices.py
@@ -1,13 +1,15 @@
-from enum import Enum
-from .mixins.choices_mixin import ChoicesMixin
+# from enum import Enum
+# from .mixins.choices_mixin import ChoicesMixin
+from django.utils.translation import gettext_lazy as _
 from apps.user_profiles.models import Profile
 
 
-class FullProfileName:
-    profiles = Profile.objects.all()
-    choices = [(f'{p.first_name} {p.last_name}', f'{p.first_name} {p.last_name}') for p in profiles]
-    
 
-    # @property
-    # def full_names(self):
-    #     return [f'{profile.first_name} {profile.last_name}' for profile in self.profiles]
+def full_profile_name():
+    try:
+        profiles = Profile.objects.all()
+        choices = [(f'{p.first_name} {p.last_name} {p.position}', f'{p.first_name} {p.last_name} {p.position}') for p in profiles]
+        choices += [(_('Not Present'), _('Not Present'))]
+    except:
+        choices = [(_('Not Present'), _("Not Present"))]
+    return choices
