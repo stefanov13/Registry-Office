7a60bd4923873e335b5c658cf8b2246812e0d942
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2023-07-26 17:41:07 +0300

    fixes in outgoing dashboard

--- registry_office/apps/common/views.py
+++ registry_office/apps/common/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render
 from django.views import generic as view
 from ..outgoing_log.models import OutgoingLogModel
@@ -12,3 +13,17 @@ from ..outgoing_log.models import OutgoingLogModel
 class OutgoingDashboardView(view.ListView):
     template_name = 'common/outgoing-dashboard.html'
     model = OutgoingLogModel
+
+    def get_queryset(self):
+        # Order items by primary key in descending order
+        return self.model.objects.order_by('-pk')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Modify the 'document_img' field in the context to display only the file name
+        documents = context['object_list']
+        for document in documents:
+            document.document_img = os.path.basename(document.document_img.name)
+
+        return context
