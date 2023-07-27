9b6ceea34c634ea773f0084b398d639e2d07cdb0
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2023-07-28 01:12:38 +0300

    fix some functionality

--- registry_office/apps/outgoing_log/admin.py
+++ registry_office/apps/outgoing_log/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import OutgoingLogModel
 
 # Register your models here.
+
+@admin.register(OutgoingLogModel)
+class OutgoingLogAdmin(admin.ModelAdmin):
+    pass
