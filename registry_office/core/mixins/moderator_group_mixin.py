3f1ccec940efc1c033a3d875650ff71d35b62ba7
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2023-07-31 18:19:12 +0300

    fix outgoing user's permissions

--- registry_office/core/mixins/moderator_group_mixin.py
+++ registry_office/core/mixins/moderator_group_mixin.py
@@ -7,7 +7,7 @@ class GroupRequiredMixin(AccessMixin):
     def dispatch(self, request, *args, **kwargs):
         user_groups = set(request.user.groups.values_list('name', flat=True))
 
-        if not all([user_groups.intersection(set(self.allowed_groups)), request.user.is_superuser, request.user.is_staff]):
+        if not any([user_groups.intersection(set(self.allowed_groups)), request.user.is_superuser, request.user.is_staff]):
             raise Http404()
         
         return super().dispatch(request, *args, **kwargs)
