5a9322dee7799b86bad5c5fb73d11b0df335400f
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2023-07-31 18:17:09 +0300

    fix outgoing user's permissions

--- registry_office/apps/common/views.py
+++ registry_office/apps/common/views.py
@@ -3,30 +3,13 @@ from django.shortcuts import render
 from django.views import generic as view
 from ..outgoing_log.models import OutgoingLogModel
 from ..incoming_log.models import IncomingLogModel
+from ..user_profiles.models import Profile
 
 # Create your views here.
 
 
 class BaseTemplateView(view.TemplateView):
     template_name = 'common/index.html'
-
-class OutgoingDashboardView(view.ListView):
-    template_name = 'common/outgoing-dashboard.html'
-    model = OutgoingLogModel
-
-    def get_queryset(self):
-        # Order items by primary key in descending order
-        return self.model.objects.order_by('-pk')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        # Modify the 'document_img' field in the context to display only the file name
-        documents = context['object_list']
-        for document in documents:
-            document.document_img = os.path.basename(document.document_img.name)
-
-        return context
 
 class IncomingDashboardView(view.ListView):
     template_name = 'common/incoming-dashboard.html'
@@ -45,3 +28,24 @@ class IncomingDashboardView(view.ListView):
             document.document_img = os.path.basename(document.document_img.name)
 
         return context
+
+class OutgoingDashboardView(view.ListView):
+    template_name = 'common/outgoing-dashboard.html'
+    model = OutgoingLogModel
+    extra_content = Profile.objects.all()
+
+    def get_queryset(self):
+        # Order items by primary key in descending order
+        return self.model.objects.order_by('-pk')
+
+    def get_context_data(self, **kwargs):
+        
+        context = super().get_context_data(**kwargs)
+
+        # Modify the 'document_img' field in the context to display only the file name
+        documents = context['object_list']
+        for document in documents:
+            document.document_img = os.path.basename(document.document_img.name)
+
+        return context
+
