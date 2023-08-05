552269539f6a2e91a1b8b1eb269c79db2e6f2143
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2023-08-05 09:48:26 +0300

    some fixes in news feed app

--- registry_office/core/mixins/max_length_choices_strings_mixin.py
+++ registry_office/core/mixins/max_length_choices_strings_mixin.py
@@ -3,4 +3,4 @@ from .choices_mixin import ChoicesMixin
 class MaxLengthChoicesStringsMixin(ChoicesMixin):
     @classmethod
     def max_length(cls):
-        return max(len(x.value) for x in cls) + 1
+        return max(len(x.value) for x in cls)
