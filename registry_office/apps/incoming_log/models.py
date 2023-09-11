26228bcab007abc6bd973e98d9a57d498125a46a
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2023-09-11 17:10:33 +0300

    fix log_num generation

--- registry_office/apps/incoming_log/models.py
+++ registry_office/apps/incoming_log/models.py
@@ -73,7 +73,7 @@ class IncomingLogModel(models.Model):
     def save(self, *args, **kwargs):
         if not self.log_num:
             # Auto-generate the log_num value on first save
-            last_instance = IncomingLogModel.objects.order_by('-log_num').first()
+            last_instance = IncomingLogModel.objects.order_by('-creation_date', '-log_num').first()
             
             current_year = timezone.now().year
 
