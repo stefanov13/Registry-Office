bb1106d26f6bc6c590d2c9bd203b0db14f0c15fd
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2024-01-26 19:20:12 +0200

    Register models in contracts log app

--- registry_office/apps/contracts_log/admin.py
+++ registry_office/apps/contracts_log/admin.py
@@ -1,23 +1,37 @@
 from django.contrib import admin
-from .models import ContractTypesModel, GeneralContractsLogModel, EducationContractsLogModel
+from . import models
 
-# Register your models here.
-@admin.register(ContractTypesModel)
+
+@admin.register(models.ContractTypesModel)
 class ContractTypesAdmin(admin.ModelAdmin):
     ordering = ('-pk',)
     list_display = ('contract_type',)
     list_filter = ('contract_type',)
     search_fields = ('contract_type',)
 
-@admin.register(GeneralContractsLogModel)
+@admin.register(models.GeneralContractsLogModel)
 class GeneralContractsLogAdmin(admin.ModelAdmin):
     ordering = ('-pk',)
     list_display = ('log_num', 'creation_date', 'title',)
     list_filter = ('log_num', 'creation_date', 'title',)
     search_fields = ('log_num', 'creation_date', 'title',)
 
-@admin.register(EducationContractsLogModel)
+@admin.register(models.EducationContractsLogModel)
 class EducationContractsLogAdmin(admin.ModelAdmin):
+    ordering = ('-pk',)
+    list_display = ('log_num', 'creation_date', 'title',)
+    list_filter = ('log_num', 'creation_date', 'title',)
+    search_fields = ('log_num', 'creation_date', 'title',)
+
+@admin.register(models.FreelanceContractsLogModel)
+class FreelanceContractsLogAdmin(admin.ModelAdmin):
+    ordering = ('-pk',)
+    list_display = ('log_num', 'creation_date', 'title',)
+    list_filter = ('log_num', 'creation_date', 'title',)
+    search_fields = ('log_num', 'creation_date', 'title',)
+
+@admin.register(models.FreelanceLectureContractsLogModel)
+class FreelanceLectureContractsLogAdmin(admin.ModelAdmin):
     ordering = ('-pk',)
     list_display = ('log_num', 'creation_date', 'title',)
     list_filter = ('log_num', 'creation_date', 'title',)
