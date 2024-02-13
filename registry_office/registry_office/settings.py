ccae846e293b9f7d854c452a40d5de4a23b12ba7
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2024-02-13 18:12:03 +0200

    Update settings.py

--- registry_office/registry_office/settings.py
+++ registry_office/registry_office/settings.py
@@ -188,3 +188,35 @@ LOGOUT_REDIRECT_URL = reverse_lazy('login')
 LOGIN_URL = reverse_lazy('login')
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
+#     "root": {"level": "INFO", "handlers": ["file"]},
+#     "handlers": {
+#         "file": {
+#             "level": "INFO",
+#             "class": "logging.handlers.RotatingFileHandler",
+#             "filename": "/var/log/django.log",
+#             'maxBytes' : 1024*1024*10, # 10MB
+#             'backupCount' : 10,
+#             "formatter": "app",
+#         },
+#     },
+#     "loggers": {
+#         "django": {
+#             "handlers": ["file"],
+#             "level": "INFO",
+#             "propagate": True
+#         },
+#     },
+#     "formatters": {
+#         "app": {
+#             "format": (
+#                 u"%(asctime)s [%(levelname)-8s] "
+#                 "(%(module)s.%(funcName)s) %(message)s"
+#             ),
+#             "datefmt": "%Y-%m-%d %H:%M:%S",
+#         },
+#     },
+# }
