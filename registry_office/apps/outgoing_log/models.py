be5a9dfc70b29d8c8a00723d17e99446bf853336
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2024-02-29 16:59:21 +0200

    Fix bug in order by criteria

--- registry_office/apps/outgoing_log/models.py
+++ registry_office/apps/outgoing_log/models.py
@@ -74,7 +74,7 @@ class OutgoingLogModel(models.Model):
     def save(self, *args, **kwargs):
         if not self.log_num:
             # Auto-generate the log_num value on first save
-            last_instance = OutgoingLogModel.objects.order_by('-creation_date', '-log_num').first()
+            last_instance = OutgoingLogModel.objects.order_by('-creation_date__date', '-log_num').first()
 
             current_year = timezone.now().year
 
